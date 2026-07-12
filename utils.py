99743c3b7a02c048b7df5487d0cfafe1a4f925ca
mait62 <maitrayibhardwaj62@gmail.com>
2026-07-12 22:51:35 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def format_confidence(confidence):
-    """Turns 0.87 into '87%'"""
-    return f"{confidence * 100:.0f}%"
-
-def capitalize_label(label):
-    """Turns 'cell phone' into 'Cell Phone'"""
-    if label is None:
-        return "Unknown"
-    return label.title()
